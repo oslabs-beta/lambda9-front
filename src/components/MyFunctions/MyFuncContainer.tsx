6f6654d81991684ccf54cfe4715640f2b05f1efc
Esther Lee <37000206+esthleej@users.noreply.github.com>
2019-07-11 16:31:58 -0700

    add myfunc routes

--- src/components/MyFunctions/MyFuncContainer.tsx
+++ src/components/MyFunctions/MyFuncContainer.tsx
@@ -3,12 +3,71 @@ import React from 'react';
 import { MyContext } from '../../App';
 import console = require('console');
 
-const MyFuncContainer: React.FunctionComponent<{}> = () => {
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  RouteComponentProps
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+type TParams = { func: string };
+
+// const MyFuncContainer: React.FunctionComponent<{}> = () => {
+//   return (
+//     <MyContext.Consumer>
+//       {context => <div>{JSON.stringify(context.state)}</div>}
+//     </MyContext.Consumer>
+//   );
+// };
+
+const Info = styled.div`
+  display: flex;
+`;
+
+function MyFuncContainer({ match }: RouteComponentProps<TParams>) {
   return (
     <MyContext.Consumer>
-      {context => <div>{context.state.functions.functionName}</div>}
+      {context => (
+        <div>
+          <div>
+            <Info>
+              <div>Name:</div>
+              <div>
+                {JSON.stringify(
+                  context.state.functions.filter(ele => {
+                    return ele.functionName === match.params.func;
+                  })[0].functionName
+                )}
+              </div>
+            </Info>
+          </div>
+          <Info>
+            <div>Inovcation:</div>
+            <div>
+              {JSON.stringify(
+                context.state.functions.filter(ele => {
+                  return ele.functionName === match.params.func;
+                })[0].invocation
+              )}
+            </div>
+          </Info>
+          <Info>
+            <div>Error:</div>
+            <div>
+              {JSON.stringify(
+                context.state.functions.filter(ele => {
+                  return ele.functionName === match.params.func;
+                })[0].error
+              )}
+            </div>
+          </Info>
+        </div>
+      )
+      // <h2>This is a page for product with ID: {match.params.func}</h2>
+      }
     </MyContext.Consumer>
   );
-};
+}
 
 export default MyFuncContainer;
