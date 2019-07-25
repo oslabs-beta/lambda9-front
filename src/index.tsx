8642cff5a1c4af1643633fb844ac3fbaa4f04a22
Jushuworld <jushuworld@gmail.com>
2019-07-25 10:20:03 -0700

    jun final

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,18 @@ import Amplify from "aws-amplify";
 import configuration from "./aws-exports.js";
 import appSyncConfiguration from "./aws-appsync-config.js";
 
-Amplify.configure({ ...configuration, ...appSyncConfiguration });
+Amplify.configure({
+  ...configuration,
+  ...appSyncConfiguration,
+  Auth: {
+    identityPoolId: "us-east-1:59c956fb-7dff-4bd7-b370-9b3a36596a09", //REQUIRED - Amazon Cognito Identity Pool ID
+    region: "US_EAST_1"
+  },
+  Storage: {
+    AWSS3: {
+      bucket: "airfn-user-profile"
+    }
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
